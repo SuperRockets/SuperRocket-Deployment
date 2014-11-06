bf5b58a78885784ad282f608a04101582922c1af
yeongseok <yeongseok.jang@h9works.com>
2014-11-06 16:24:30 +0900

    add java, tomcat install add restart_web method

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from fabric.decorators import task, roles
 from fabric.state import env
 from fabtools import require
+import fabtools
 
 __author__ = 'yeongseokjang'
 
@@ -12,16 +13,24 @@ env.home = '/home/' + env.user
 env.project_name = 'superrocketserver'
 env.project_path = env.home + '/' + env.project_name
 env.static_path = env.home + '/static'
-env.key_filename = 'SuperRocket-DEV.pem'
+env.key_filename = 'SuperRocekt-DEV.pem'
 
 @task
 @roles('web')
 def setup_web():
+    require.oracle_jdk.installed(version='8u25-b17')
+
+    require.tomcat.installed(version='8.0.14')
+    require.service.started()
+
     require.nginx.server()
     require.nginx.site(env.project_name, template_source='nginx-superrocket.site',
                        port=80,
                        server_alias='',
                        static_path=env.static_path)
     require.nginx.disable('default')
-
     require.directory(env.home + '/logs')
+
+def restart_web():
+    require.service.restart('tomcat')
+    require.service.restart('nginx')
